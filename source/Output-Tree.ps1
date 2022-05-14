a39f01537aa8c1b7b5956f4ef90b28d51beb2627
ao-iro <44068304+ao-iro@users.noreply.github.com>
2022-05-14 22:10:13 +0900

    ight click to execute tree command

---
+++ source/Output-Tree.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [String]
+    $targetPath
+)
+
+$logPath="C:\Users\xxxx\"
+
+$dateTime = (Get-Date).ToString("yyyyMMddHHmmss")
+$treeFile = $logPath + $dateTime + "_output.txt"
+tree /F "" + $targetPath + "" > $treeFile
+
+notepad $treeFile
